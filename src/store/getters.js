6e0f6c210b21967eb57d6e707ca29d210f46a666
Leo927 <asd347925290@gmail.com>
2021-02-04 07:38:32 -0600

    added comment of how to enable access control

--- src/store/getters.js
+++ src/store/getters.js
@@ -3,6 +3,11 @@ export default{
         return true
     },
 
+    // use the following for access control
+    // isAdmin: (state)=>{
+    //     return state.admins.includes(state.uid)
+    // },
+
     itemArray: (state)=>{
         return state.items
     }
